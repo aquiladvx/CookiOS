391a0aef5cf6234fc4fa6df20cfeefb61e367750
Davi Aquila <daviaquila162@gmail.com>
2022-03-18 23:25:46 -0400

    init

---
+++ cook/cookApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookApp.swift
+//  cook
+//
+//  Created by Davi Aquila on 2022-03-18.
+//
+
+import SwiftUI
+
+@main
+struct cookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
